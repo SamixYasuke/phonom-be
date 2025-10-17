1c300e1956e7e372c044910a108fafc719df36ab
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2025-10-17 17:26:45 +0100

    Add root route to return success message
    Added a root route that returns a success message.

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/v1", sectionRouter);
+app.get("/", ((req, res)=>{
+  res.status(200).json({
+    message: "Hello, It works"
+  })
+})
 app.use((err, req, res, next) => {
   console.error(err.stack);
   const statusCode = err.statusCode || 500;
@@ -23,3 +28,4 @@ app.use((err, req, res, next) => {
 });
 
 connectDB(app);
+
